2bd75f20da5d9985df3bf341282562ca22f04f46
arul <bahrulhikmI@gmail.com>
2021-07-08 09:38:35 +0800

    added JWT Strategy for signin

--- src/auth/auth.controller.ts
+++ src/auth/auth.controller.ts
@@ -1,6 +1,9 @@
 import { AuthCredentialsDto } from './dto/auth-credentials.dto';
-import { Body, Controller, Post, ValidationPipe } from '@nestjs/common';
+import { Body, Controller, Post, Req, UseGuards, ValidationPipe } from '@nestjs/common';
 import { AuthService } from './auth.service';
+import { AuthGuard } from '@nestjs/passport';
+import { User } from './user.entity';
+import { GetUser } from './get-user.decorator';
 
 @Controller('auth')
 export class AuthController {
@@ -15,6 +18,12 @@ export class AuthController {
     @Post('/signin')
     signIp(@Body(ValidationPipe) authCredentialsDto:AuthCredentialsDto): Promise<{accessToken: string}> {
         return this.authService.signIn(authCredentialsDto);
+    }
+
+    @Post('/test')
+    @UseGuards(AuthGuard())
+    test(@GetUser() user: User){
+        console.log(user);
     }
 
 
